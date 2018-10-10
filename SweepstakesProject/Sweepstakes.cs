42eae5205e943e5b6356082dfc2ea3df77eb0f6f
Traumatthew <43039716+Traumatthew@users.noreply.github.com>
2018-10-10 15:13:08 -0500

    initial commit

---
+++ SweepstakesProject/Sweepstakes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProject
+{
+    class Sweepstakes
+    {
+        //use Dictonary Data Structure
+        //-	Sweepstakes(string name)
+        //-	void RegisterContestant(Contestant contestant)
+        //-	string PickWinner()
+        //-	void PrintContestantInfo(Contestant contestant)
+    }
+}
